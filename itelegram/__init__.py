0461c04c1fd453562a4af530a59e099b99a37496
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-10-22 15:28:12 +0330

    Bump Version to 1.0.7

--- itelegram/__init__.py
+++ itelegram/__init__.py
@@ -6,5 +6,5 @@ class GlobalHandler(Handler):
         return True
 
 
-__version__ = "1.0.6"
-default_app_config = "django_itelegram.apps.DjangoTelegramBot"
+__version__ = "1.0.7"
+default_app_config = "itelegram.apps.DjangoTelegramBot"
