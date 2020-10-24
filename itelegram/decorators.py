112bc9b856cd92c9c6010b77135a4beebca5525a
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-10-24 19:15:45 +0330

    Update `telegram_perm` decorator based on `site_user`

--- itelegram/decorators.py
+++ itelegram/decorators.py
@@ -1,11 +1,11 @@
 from functools import wraps
 
 from django.conf import settings
-from django.contrib.auth.models import User
 from telegram import Update
 from telegram.ext import CallbackContext, DispatcherHandlerStop
 
 from .defaults import telegram_not_logged_in_text, telegram_permission_denied_text, telegram_parse_mode
+from .models import TelegramUser
 
 
 def user_passes_test(test_func):
@@ -14,15 +14,17 @@ def user_passes_test(test_func):
         def _wrapped_view(update: Update, context: CallbackContext, *args, **kwargs):
             chat_id = update.message.from_user.id
             try:
-                user = User.objects.get(telegram_id=chat_id)
-            except User.DoesNotExist:
+                user = TelegramUser.objects.get(id=chat_id)
+                if not user.site_user:
+                    raise TelegramUser.DoesNotExist
+            except TelegramUser.DoesNotExist:
                 context.bot.send_message(
                     chat_id=chat_id,
                     text=getattr(settings, "TELEGRAM_NOT_LOGGED_IN_TEXT", telegram_not_logged_in_text),
                     parse_mode=getattr(settings, "TELEGRAM_PARSE_MODE", telegram_parse_mode),
                 )
                 raise DispatcherHandlerStop
-            if test_func(user):
+            if test_func(user.site_user):
                 return view_func(update, context, *args, **kwargs)
             context.bot.send_message(
                 chat_id=chat_id,
